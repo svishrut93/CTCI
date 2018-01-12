3c71f9f84d70cb7b39e7ed8107d96a1b4d91d367
Vishrut Sharma <s.vishrut93@gmail.com>
2018-01-12 08:46:45 -0800

    Update StackWithMin.java

--- Min operation on stack in O-1/StackWithMin.java
+++ Min operation on stack in O-1/StackWithMin.java
@@ -1,6 +1,9 @@
 package questionthreepointtwo;
 
+//Question 3.2
 
+//How would you design a stack which, in addition to push and pop, also has a function min which returns the minimum element?
+//Push, Pop and min should all operate in O (1) time.
 
 public class StackWithMin extends Stack {
 	
