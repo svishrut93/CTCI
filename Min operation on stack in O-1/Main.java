4edc29c8ad29d768f949547e1a38fb0960d696e3
Vishrut Sharma <s.vishrut93@gmail.com>
2018-01-12 08:44:32 -0800

    Update Main.java

--- Min operation on stack in O-1/Main.java
+++ Min operation on stack in O-1/Main.java
@@ -1,5 +1,10 @@
 package questionthreepointtwo;
 
+//Question 3.2
+
+//How would you design a stack which, in addition to push and pop, also has a function min which returns the minimum element?
+//Push, Pop and min should all operate in O (1) time.
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -41,48 +46,6 @@ public class Main {
 		System.out.println("Elements is stack are : ");
 		ss.display();
 		
-		
-		//First.display(); 
-		
-		
-		//Remove elements : all  test cases 
-//		First.remove(10);
-//		First.display();
-//		First.remove(20);
-//		First.display();
-//		First.remove(30);
-//		First.display();
-//		First.remove(40);
-//		First.remove(888);
-//		First.display();
-//		First.remove(50);
-//		First.display();
-//		First.Size();
-		
-		//Remove by position : all test cases 
-//		First.removefromposition(0);
-//		First.removefromposition(155);
-//		First.removefromposition(1);
-//		First.display();
-//		First.removefromposition(2);
-//		First.display();
-//		First.removefromposition(3);
-//		First.display();
-//		First.removefromposition(4);
-//		First.display();
-//		First.removefromposition(5);
-//		First.display(); 
-		
-	
-		//Test cases for different add positions : 
-//		First.add(100, 1);
-//		First.display();
-//		First.add(200,3);
-//		First.display();
-//		First.add(300,5);
-//		First.display();
-//		First.add(0,1000);
-//		First.add(1000,0);
 		
 	}
 		
