402c1f5d767f70834b864a1f9894f5c98cf63324
Vishrut Sharma <s.vishrut93@gmail.com>
2019-07-31 16:25:28 -0700

    Update ChangeToZeros.java

--- ChangeToZeros.java
+++ ChangeToZeros.java
@@ -5,8 +5,6 @@ import java.util.* ;
 //Write an algorithm such that if an element in an MxN matrix is 0, its entire 
 //row and column are set to 0 
 
-
-
 public class ChangeToZeros {
 	
 	public static void main(String[] args) {
